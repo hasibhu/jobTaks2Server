bff91b5f12b21ea22739d5786fedb5181b9611d4
hasibhu <hasibul1987@gmail.com>
2024-08-16 10:09:37 +0200

    mongodb uri has been added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+require("dotenv").config();
 const port = process.env.PORT || 5007;
 
 
@@ -10,7 +12,16 @@ app.use(bodyParser.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qoryues.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 
 
