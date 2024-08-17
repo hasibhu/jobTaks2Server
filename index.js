84a4d7095fea59dd8ccc9a735c77a448ee61c97e
hasibhu <hasibul1987@gmail.com>
2024-08-17 14:36:14 +0200

    variable name has been adjusted

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5007;
 
 // 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://jobtask2-49c6e.web.app' ],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://jobtask2-49c6e.web.app' ],
     credentials: true,
     optionSuccessStatus: 200,
 };
@@ -50,8 +50,59 @@ const productCollection = client.db("jobTask2").collection("products");
 
 app.get('/products', async(req, res) => {
     try {
-        const result = await productCollection.find().toArray();
-        res.json(result);
+        const data = parseInt(req.query.page);
+      const price = req.query.price;
+      const company = req.query.brand;
+      const newest = req.query.new;
+      const category = req.query.category;
+      const search = req.query.search;
+      const brandArr = brand ? brand.split(",") : [];
+      const categoryArr = category ? category.split(",") : [];
+      const priceRange = parseInt(req.query.range);
+
+      let query = {};
+
+      if (brandArr?.length > 0) {
+        query.brandName = { $in: brandArr };
+      }
+      if (categoryArr?.length > 0) {
+        query.category = { $in: categoryArr };
+      }
+
+      if (priceRange !== null) {
+        query.price = { $gte: priceRange - 49, $lte: priceRange };
+      }
+
+      if (search) {
+        query.productName = { $regex: search, $options: "i" };
+      }
+      
+      // sort by price
+      let sortOption = {};
+      if (price === "Low_To_High") {
+        sortOption.price = 1;
+      } else if (price === "High_To_Low") {
+        sortOption.price = -1;
+      }
+      if (newest) {
+        sortOption.dateTime = -1;
+      }
+
+      const limit = 9;
+      const skip = (page - 1) * limit;
+
+      try {
+        const count = await products.find(query).sort(sortOption).toArray();
+
+        const allProducts = await products
+          .find(query)
+          .sort(sortOption)
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+        res.status(200).json({ allProducts, totalCount: count.length });
+﻿
+
     } catch (error) {
         console.error("Error fetching jobs:", error);
         res.status(500).json({ error: "Internal server error" });
